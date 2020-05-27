21b434378a2eb474254d480882650ddd16b321dd
oterpater <oteropaternina.n@gmail.com>
2020-05-27 08:14:33 -0500

    updating app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Register from './pages/Register';
+import Stores from './pages/Stores';
+import Maps from './pages/Map';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Register} />
+          <Route exact path="/stores" component={Stores} />
+          <Route exact path="/map" component={Maps} />
+        </Switch>
+      </Router>
     </div>
   );
 }
